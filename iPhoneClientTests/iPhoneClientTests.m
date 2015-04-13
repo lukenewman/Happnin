55b1a287e06eaaa46deedd9db1b0e4c32a514ecf
lukenewman <lukenewman@bellsouth.net>
2015-04-13 12:16:50 +0200

    Fixed place list images loading, need to tweak place detail and media list

--- iPhoneClientTests/iPhoneClientTests.m
+++ iPhoneClientTests/iPhoneClientTests.m
@@ -1,6 +1,6 @@
 //
-//  HappninTests.m
-//  HappninTests
+//  iPhoneClientTests.m
+//  iPhoneClientTests
 //
 //  Created by Luke Newman on 2/26/15.
 //  Copyright (c) 2015 Luke Newman. All rights reserved.
@@ -9,11 +9,11 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface HappninTests : XCTestCase
+@interface iPhoneClientTests : XCTestCase
 
 @end
 
-@implementation HappninTests
+@implementation iPhoneClientTests
 
 - (void)setUp {
     [super setUp];
